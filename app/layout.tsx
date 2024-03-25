e2df5fcac40f747bed04014515cc54e794ec5f4b
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-03-24 21:54:32 -0300

    feat: add auth provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
+import { getServerSession } from "next-auth";
+import AuthProvider from "@/utils/SessionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,19 +13,22 @@ export const metadata: Metadata = {
   description: "Controle de vagas e contratações",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession();
   return (
     <html lang="pt-BR">
       <body className={inter.className}>
-        <div className="mx-auto max-w-5xl text-2xl gap-2 mb-10">
-          <Navbar />
-          {children}
-          <Toaster />
-        </div>
+        <AuthProvider session={session}>
+          <div className="mx-auto max-w-5xl text-2xl gap-2 mb-10">
+            <Navbar />
+            {children}
+            <Toaster />
+          </div>
+        </AuthProvider>
       </body>
     </html>
   );
