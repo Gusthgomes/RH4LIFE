265eb1766aa538242807c4b76b29bc663cdcdcde
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-03-24 21:56:11 -0300

    chore: add new link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,19 +3,24 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="w-full h-full py-2 px-2 mt-2 rounded mb-4 bg-blue-400">
       <ul className="flex justify-between m-10 items-center">
         <div>
-          <Link href="/">Home</Link>
+          <Link className="text-black hover:text-white" href="/">
+            Home
+          </Link>
         </div>
         <div className="flex gap-10">
-          <Link href="login">
+          <Link className="text-black hover:text-white" href="login">
             <li>Login</li>
           </Link>
-          <Link href="register">
+          <Link className="text-black hover:text-white" href="register">
             <li>Cadastre-se</li>
           </Link>
-          <Link href="about">
+          <Link className="text-black hover:text-white" href="about">
+            <li>Vagas</li>
+          </Link>
+          <Link className="text-black hover:text-white" href="about">
             <li>Sobre</li>
           </Link>
         </div>
