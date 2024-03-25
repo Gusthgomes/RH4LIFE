a6a9203286cce6cc7e38246961a5b6c561c44990
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-03-24 21:56:34 -0300

    chore: add role with new schema to user

--- models/User.ts
+++ models/User.ts
@@ -3,6 +3,11 @@ import { Schema, models, model } from "mongoose";
 const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: false },
+  role: { type: String, default: "user" },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const User = models.User || model("User", UserSchema);
