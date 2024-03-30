cc96af3a07894e95f53b523517975192d2423593
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-03-30 20:24:16 -0300

    feat: add callback function

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,10 @@
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
+import CredentialsProvider, { CredentialsConfig } from "next-auth/providers/credentials";
 import User from "@/models/User";
 import { connectToDatabase } from "@/utils/db";
 import bcrypt from "bcryptjs";
 import NextAuth from "next-auth/next";
+import { Account, User as AuthUser } from "next-auth";
 
 type Credential = {
   email: string;
@@ -39,7 +40,33 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+
+  callbacks: {
+    async signIn({ user, account}: { user: AuthUser, account: Account}) {
+      if( account?.provider == "credentials") {
+        return true;
+      };
+      if (account?.provider == "google") {
+        await connectToDatabase();
+        try {
+          const existUser = await User.findOne({email: user.email});
+          if(!existUser) {
+            const newUser = new User ({
+              email: user.email,
+            })
+
+            await newUser.save();
+            return true;
+          }
+          return true;
+        } catch (error) {
+          console.log("Erro: ", error)
+        }
+      }
+    },
+  },
 };
 
+// @ts-ignore
 export const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
