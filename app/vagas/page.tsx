d6ff19d1bc8e42fbdadc5cbd848dcc081073ccff
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-04-10 20:50:07 -0300

    feat: implement useForm and zod validation, add form to create new vaga

--- app/vagas/page.tsx
+++ app/vagas/page.tsx
@@ -1,122 +1,144 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useToast } from "@/components/ui/use-toast";
+
+const schema = z.object({
+  name: z.string().min(3, "O nome da vaga deve ter no mínimo 3 caracteres").max(50, "O nome da vaga deve ter no máximo 50 caracteres"),
+  description: z.string().min(3, "A descrição da vaga deve ter no mínimo 3 caracteres").max(500, "A descrição da vaga deve ter no máximo 500 caracteres"),
+  location: z.string().min(3, "A localização da vaga deve ter no mínimo 3 caracteres").max(50, "A localização da vaga deve ter no máximo 50 caracteres"),
+  category: z.string(),
+  benefits: z.string().max(500, "Os benefícios da vaga devem ter no máximo 500 caracteres"),
+})
+
+type FormData = z.infer<typeof schema>;
 
 const Vagas = () => {
+
+  const router = useRouter();
+
   const session = useSession();
 
-  const [formData, setFormData] = useState({
-    name: '',
-    description: '',
-    location: '',
-    category: '',
-    benefits: '',
-  });
+  const { toast } = useToast();
 
   if (!session.data) {
     redirect("/login");
-  }
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
-    setFormData(prevState => ({
-      ...prevState,
-      [name]: value,
-    }));
   };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+
+  const onSubmit = async (data: FormData) => {
+    const { name, description, location, category, benefits } = data;
+
     try {
-      // Enviar os dados do formulário para o backend
-      const response = await fetch('/api/vagas', {
-        method: 'POST',
+      const response = await fetch("/api/vagas", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify({ 
+          name, 
+          description, 
+          location, 
+          category, 
+          benefits
+        }),
       });
 
-      if (response.ok) {
-        console.log('Vaga criada com sucesso');
-        // Limpar o formulário após o envio bem-sucedido
-        setFormData({
-          name: '',
-          description: '',
-          location: '',
-          category: '',
-          benefits: '',
+      if (response.status === 500) {
+        toast({
+          variant: "destructive",
+          title: "Erro ao criar a vaga!",
         });
-      } else {
-        throw new Error('Erro ao criar vaga');
-      }
-    } catch (error) {
-      console.error('Erro ao criar vaga:', error);
+        router.refresh();
+      };
+
+      if (response.status === 201) {
+        toast({
+          title: "Vaga criada com sucesso!",
+          description: "Aguardando candidaturas...",
+        });
+        router.push("/dashboard");
+      };
+
+    } catch (error: string | any) {
+      toast({
+        variant: "destructive",
+        title: "Ops! Erro interno no servidor",
+      });
+      console.log("Error: ", error)
     }
   };
 
-  
   return (
     <div className="max-w-md mx-auto bg-white rounded shadow-lg shadow-blue-700 overflow-hidden">
-      <form className="px-8 py-6" onSubmit={handleSubmit}>
+      <form className="px-8 py-6" onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-6">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
             Nome
           </label>
           <input
+          {...register("name")}
             className="w-full px-3 py-2 placeholder-gray-400 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
             type="text"
             id="name"
             name="name"
             placeholder="Digite o nome da vaga"
-            value={formData.name}
-            onChange={handleChange}
             required
           />
+          <p className="my-2">{errors.name?.message}</p>
         </div>
         <div className="mb-6">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
             Descrição
           </label>
           <textarea
+          {...register("description")}
             className="w-full px-3 py-2 placeholder-gray-400 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
             id="description"
             name="description"
             rows={4}
-            placeholder="Digite a descrição da vaga"
-            value={formData.description}
-            onChange={handleChange}
+            placeholder="Digite a descrição da vaga"          
             required
           />
+          <p className="my-2">{errors.description?.message}</p>
         </div>
         <div className="mb-6">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="location">
             Localização
           </label>
           <input
+          {...register("location")}
             className="w-full px-3 py-2 placeholder-gray-400 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
             type="text"
             id="location"
             name="location"
             placeholder="Digite a localização da vaga"
-            value={formData.location}
-            onChange={handleChange}
             required
           />
+          <p className="my-2">{errors.location?.message}</p>
         </div>
         <div className="mb-6">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="category">
             Categoria
           </label>
           <select
+          {...register("category")}
             className="w-full px-3 py-2 placeholder-gray-400 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
             id="category"
             name="category"
-            value={formData.category}
-            onChange={handleChange}
             required
           >
             <option className=" placeholder-gray-400" value="">Selecione uma categoria</option>
@@ -126,20 +148,21 @@ const Vagas = () => {
             <option className=" placeholder-gray-400" value="Contrução Pesada">Contrução Pesada</option>
             <option className=" placeholder-gray-400" value="Stefani">Stefani</option>
           </select>
+          <p className="my-2">{errors.category?.message}</p>
         </div>
         <div className="mb-6">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="benefits">
             Benefícios
           </label>
           <input
+          {...register("benefits")}
             className="w-full px-3 py-2 placeholder-gray-400 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
             type="text"
             id="benefits"
             name="benefits"
             placeholder="Digite os benefícios da vaga"
-            value={formData.benefits}
-            onChange={handleChange}
           />
+          <p className="my-2">{errors.benefits?.message}</p>
         </div>
         <Button
           className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-600"
