1843d4716fd916ec5e0225a6d95d3b49b3be378a
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-04-14 22:01:27 -0300

    fix: remove imports

--- components/Dashboard/dashboard.tsx
+++ components/Dashboard/dashboard.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
-import Vagas from '@/models/Vagas';
 
 interface Vaga {
     _id: string;
