312a043043c4a8a2d6c1e359cb3b90e422b5ad5a
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-04-22 21:24:16 -0300

    feat: add responsiveness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,16 +3,17 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
 import { useSession, signOut } from "next-auth/react";
+import MobileNav from "./Mobile/MobileNav";
 
 const Navbar = () => {
   const session = useSession();
   return (
-    <div className="w-full h-full py-2 px-2 mt-2 rounded mb-4 bg-blue-400">
+    <div className="w-[80%] mx-auto md:w-[100%] h-full p-2 mt-2 mb-4 rounded bg-blue-400">
       <ul className="flex justify-between m-10 items-center">
         {session.status === "authenticated" ? (
           <></>
         ) : (
-          <div>
+          <div className="w-full hidden md:block">
             <Link className="text-black hover:text-white" href="/">
               Home
             </Link>
@@ -20,18 +21,16 @@ const Navbar = () => {
         )}
         {session.status === "authenticated" ? (
           <div className="w-full flex flex-row items-center justify-between gap-10">
-            <p>{session?.data?.user?.email}</p>
-            <Link className="text-black hover:text-white" href="/dashboard">
-              <li>Dashboard</li>
-            </Link>
-            <Link className="text-black hover:text-white" href="/vagas">
-              <li>Vagas</li>
-            </Link>
-            <Button className="w-18 text-2xl bg-black rounded text-white p-2 duration-7000 hover:scale-125"
-            onClick={() => signOut()}
-            >
-              Logout
-            </Button>
+            <p className="text-base md:text-2xl md:font-semibold flex text-start mr-auto">{session?.data?.user?.email}</p>
+            <div className="flex flex-row gap-3 items-center justify-center">
+              <MobileNav/>
+              <Button className="w-18 h-12 text-2xl bg-black rounded text-white p-2 transition duration-7000 hover:scale-105 hidden md:block"
+              onClick={() => signOut()}
+              >
+                Logout
+              </Button>
+            </div>
+            
           </div>
         ) : (
           <div className="flex gap-10">
