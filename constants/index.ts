a35aae0e3f24b750adebd2655215d21d667992c2
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-04-22 21:28:07 -0300

    chore: header link created

---
+++ constants/index.ts
@@ -0,0 +1,14 @@
+export const headerLink = [
+    {
+        label: 'Home',
+        route: "/",
+    },
+    {
+        label: 'Dashboard',
+        route: "/dashboard",
+    },
+    {
+        label: 'Vagas',
+        route: "/vagas",
+    },
+]
